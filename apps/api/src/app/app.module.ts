28d574b0a6bd458056fddb1a5a89d20e5be69f9d
edevargas <sailedvc@gmail.com>
2022-09-10 23:51:16 -0500

    Configure swagger

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MechanicServicesModule } from '../mechanic-services/mechanic-services.module';
+import { MechanicsModule } from '../mechanics/mechanics.module';
+import { ServicesByMechanicModule } from '../services-by-mechanic/services-by-mechanic.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MechanicsModule, MechanicServicesModule, ServicesByMechanicModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,24 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const configureSwagger = (app) => {
+  const options = new DocumentBuilder()
+    .setTitle('API de Nacho Mecánico')
+    .setDescription('REST API para Nacho Mecánico')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  configureSwagger(app);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
